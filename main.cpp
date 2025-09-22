06110dcc80f4c938f55b22d9e0bcc7d8696f7161
김희정 <heejung030619@gmail.com>
2025-09-22 09:42:30 +0900

    Initial commit

--- main.cpp
+++ main.cpp
@@ -7,13 +7,24 @@ int main() {
 	int sum = 0;
 	int multi = 0;
 
-	for (int i = 1; i <= 100; i++) {
+	for (int i = 1; i <= 100; i++) 
+	{
 		sum += i;
-		if (i % 3 == 0) {
+
+		// 프로그램 돌릴 때 *, / 연산 느림
+		if (i % 3 == 0) 
+		{
 			multi += i;
 		}
-
 	}
 	cout << sum << endl;
+	cout << multi << endl;
+
+	multi = 0;
+	// 강사님 방법
+	for (int i = 3; i <= 100; i += 3) 
+	{
+		multi += i;
+	}
 	cout << multi << endl;
 }
