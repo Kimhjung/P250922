55dcb81165488c42ca384402acf9da53efc55515
김희정 <heejung030619@gmail.com>
2025-09-22 15:12:13 +0900

    복습 및 추가설명

--- main.cpp
+++ main.cpp
@@ -1,54 +1,113 @@
-#include <iostream>
+// #inclue = 포함하다
+#include <iostream>		// C++ standerd
+#include <conio.h>		// c standard
+// #include "iostream"
 
 using namespace std;
 
-int main() {
+// 매크로
+// 1이 2보다 크다 / 참: 1반환, 거짓:2반환
+#define MAX(a,b) 1 > 2 ? 1 : 2;
 
-	int count = 0;
-	cin >> count;
-	
-	for (int i = 1; i <= count; ++i)
+int World[10][10] =
+{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+int KeyCode;
+
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
+
+char Sprites[10] = { ' ', '*' };
+
+bool bIsRunning = true;
+
+int PlayerMinX = 1;
+int PlayerMinY = 1;
+int PlayerMaxX = 8;
+int PlayerMaxY = 8;
+
+void Input()
+{
+	KeyCode = _getch();
+}
+
+void Process()
+{
+	if (KeyCode == 'W' || KeyCode == 'w')
 	{
-		for (int j = 1; j <= count - (i - 1); ++j)
-		{
-			cout << "*";
-		}
-		cout << "\n";
+		PlayerY--;
 	}
-	
-	//역방향 출력
-
-	// case 1(me)
-	for (int i = 0; i < count; ++i)
+	else if (KeyCode == 'A' || KeyCode == 'a')
 	{
-		for (int j = 0; j < 0 + i; ++j)
-		{
-			cout << " ";
-		}
-		for (int j = 0; j < count - i; ++j)
-		{
-			cout << "*";
-		}
-		cout << "\n";
+		PlayerX--;
+	}
+	else if (KeyCode == 'S' || KeyCode == 's')
+	{
+		PlayerY++;
+	}
+	else if (KeyCode == 'D' || KeyCode == 'd')
+	{
+		PlayerX++;
+	}
+	else if (KeyCode == 'Q' || KeyCode == 'q')
+	{
+		bIsRunning = false;
+	}
+}
+
+void Render()
+{
+	system("cls");
+
+	if (PlayerX < PlayerMinX) {
+		PlayerX = PlayerMinX;
+	}
+	else if (PlayerY < PlayerMinY) {
+		PlayerY = PlayerMinY;
+	}
+	else if (PlayerX > PlayerMaxX) {
+		PlayerX = PlayerMaxX;
+	}
+	else if (PlayerY > PlayerMaxY) {
+		PlayerY = PlayerMaxY;
 	}
 
-	// case 2
-	for (int i = 0; i < count; ++i)
+	for (int Y = 0; Y < 10; ++Y)
 	{
-		for (int j = 0; j < count; ++j)
+		for (int X = 0; X < 10; ++X)
 		{
-			if (i > j)
+			if (PlayerX == X && PlayerY == Y)
 			{
-				cout << " ";
+				cout << PlayerShape;
 			}
 			else
 			{
-				cout << "*";
+				cout << Sprites[World[Y][X]];
 			}
+
 		}
 		cout << "\n";
 	}
-	
+}
 
-	return 0;
+int main() {
+	//frame, deltaseconds
+	while (bIsRunning)
+	{
+		Process();
+		Render();
+		Input();
+	}
 }
