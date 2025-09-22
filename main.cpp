89e66a57d61afe6d324d966e2a1298964f322802
김희정 <heejung030619@gmail.com>
2025-09-22 16:45:12 +0900

    String 설명

--- main.cpp
+++ main.cpp
@@ -1,113 +1,22 @@
 // #inclue = 포함하다
-#include <iostream>		// C++ standerd
-#include <conio.h>		// c standard
+#include <iostream>		//C++ standerd
+#include <conio.h>		//C standard
+//#include <string>		// iostream에 들어가있음(iostream 없으면 에러 남)
 // #include "iostream"
 
 using namespace std;
 
-// 매크로
-// 1이 2보다 크다 / 참: 1반환, 거짓:2반환
-#define MAX(a,b) 1 > 2 ? 1 : 2;
-
-int World[10][10] =
+int main()
 {
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
+	// C 문자열 X, 배열을 만들어서 한 자씩 넣어야 함 / Java, C#, CPP 문자열 O
+	// utf-8 지원해줌
+	//char A[6] = { 'H','e','l','l','o','\0' };
 
-int KeyCode;
+	string Greeting = "안녕";
+	string Greeting2 = "여러분";
 
-int PlayerX = 1;
-int PlayerY = 1;
-char PlayerShape = 'P';
+	// 'A' != "A"
+	// "A" == { 'A', '\0' }
 
-char Sprites[10] = { ' ', '*' };
-
-bool bIsRunning = true;
-
-int PlayerMinX = 1;
-int PlayerMinY = 1;
-int PlayerMaxX = 8;
-int PlayerMaxY = 8;
-
-void Input()
-{
-	KeyCode = _getch();
-}
-
-void Process()
-{
-	if (KeyCode == 'W' || KeyCode == 'w')
-	{
-		PlayerY--;
-	}
-	else if (KeyCode == 'A' || KeyCode == 'a')
-	{
-		PlayerX--;
-	}
-	else if (KeyCode == 'S' || KeyCode == 's')
-	{
-		PlayerY++;
-	}
-	else if (KeyCode == 'D' || KeyCode == 'd')
-	{
-		PlayerX++;
-	}
-	else if (KeyCode == 'Q' || KeyCode == 'q')
-	{
-		bIsRunning = false;
-	}
-}
-
-void Render()
-{
-	system("cls");
-
-	if (PlayerX < PlayerMinX) {
-		PlayerX = PlayerMinX;
-	}
-	else if (PlayerY < PlayerMinY) {
-		PlayerY = PlayerMinY;
-	}
-	else if (PlayerX > PlayerMaxX) {
-		PlayerX = PlayerMaxX;
-	}
-	else if (PlayerY > PlayerMaxY) {
-		PlayerY = PlayerMaxY;
-	}
-
-	for (int Y = 0; Y < 10; ++Y)
-	{
-		for (int X = 0; X < 10; ++X)
-		{
-			if (PlayerX == X && PlayerY == Y)
-			{
-				cout << PlayerShape;
-			}
-			else
-			{
-				cout << Sprites[World[Y][X]];
-			}
-
-		}
-		cout << "\n";
-	}
-}
-
-int main() {
-	//frame, deltaseconds
-	while (bIsRunning)
-	{
-		Process();
-		Render();
-		Input();
-	}
+	cout << Greeting + " " + Greeting2 << endl;
 }
