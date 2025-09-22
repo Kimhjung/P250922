fb6949a4c9667ba2ef7a5ce62cad97b8b79f7a60
김희정 <heejung030619@gmail.com>
2025-09-22 11:27:58 +0900

    코드 리뷰 및 정리

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main() {
 
 	int count = 0;
-
 	cin >> count;
-
-	/*
+	
 	for (int i = 1; i <= count; ++i)
 	{
 		for (int j = 1; j <= count - (i - 1); ++j)
@@ -17,8 +15,10 @@ int main() {
 		}
 		cout << "\n";
 	}
-	*/
 	
+	//역방향 출력
+
+	// case 1(me)
 	for (int i = 0; i < count; ++i)
 	{
 		for (int j = 0; j < 0 + i; ++j)
@@ -32,6 +32,23 @@ int main() {
 		cout << "\n";
 	}
 
+	// case 2
+	for (int i = 0; i < count; ++i)
+	{
+		for (int j = 0; j < count; ++j)
+		{
+			if (i > j)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
+		}
+		cout << "\n";
+	}
+	
 
 	return 0;
 }
